64a2dbbf315cd12270a90280e5c56408be48a005
Samuel Werlang <54645776+Ssamueell@users.noreply.github.com>
2025-07-13 17:19:49 -0300

    main.js

--- src/main.js
+++ src/main.js
@@ -1,65 +1,88 @@
 "use strict"
 
 let shop = document.getElementById("shop");
-
+let shopItemsData = JSON.parse(localStorage.getItem("shop-stock")) || defaultShopItemsData;
 let basket = JSON.parse(localStorage.getItem("shop-products")) || [];
 
 let generateShop = () => {
-    
-    return (shop.innerHTML = shopItemsData.map((item) => {
-        let {id, name, price, desc, img} = item;
-        let search = basket.find((basketItem) => basketItem.id === id) || [];
-        return `    
-        <div id="product-id-${id}" class="item">
-            <img width="100%" height="220" src="${img}">
-            <div class="details">
-                <h3>${name}</h3>
-                <p>${desc}</p>
-                <div class="price-quantity">
-                    <h2>$${price}</h2>
-                    <div class="buttons">
-                        <i onclick="decrementCount('${id}')" class="bi bi-dash-lg"></i>
-                        <div id="${id}" class="quantity">${search.count ?? 0}</div>
-                        <i onclick="incrementCount('${id}')" class="bi bi-plus-lg"></i>
+    shop.innerHTML = shopItemsData.map((item) => {
+        let { id, name, price, desc, img } = item;
+        let search = basket.find((basketItem) => basketItem.id === id) || {};
+
+        return `
+            <div id="product-id-${id}" class="item">
+                <img width="100%" height="220" src="${img}">
+                <div class="details">
+                    <h3>${name}</h3>
+                    <p>${desc}</p>
+                    <div class="price-quantity">
+                        <h2>$${price}</h2>
+                        <h3></h3>
+                        <div class="buttons">
+                            <i onclick="decrementCount('${id}')" class="bi bi-dash-lg"></i>
+                            <div id="${id}" class="quantity">${search.count ?? 0}</div>
+                            <i onclick="incrementCount('${id}')" class="bi bi-plus-lg"></i>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    `
-    }).join('')); 
-}
+        `;
+    }).join('');
+};
+
 
 generateShop();
 
 let incrementCount = (id) => {
-    // let selectedItem = document.getElementById(id);
-    let search = basket.find(basketItem => basketItem.id === id);
-    if(!search) {
+    let product = shopItemsData.find(prod => prod.id === id);
+    let basketItem = basket.find(item => item.id === id);
+
+    if(product.stock <= 0) {
+        return alert("No Stock!")
+    }
+
+    if(!basketItem) {
         basket.push({
             id: id,
-            count : 1
+            count : 1,
+            stock : product.stock - 1
         })
+        product.stock -= 1;
     } else {
-        search.count += 1;
-    }
+        basketItem.count += 1;
+        basketItem.stock -= 1;
+        product.stock -=1;
+    };
+
     update(id);
     localStorage.setItem("shop-products", JSON.stringify(basket));
+    localStorage.setItem("shop-stock", JSON.stringify(shopItemsData));
 };
 
+
+
+
 let decrementCount = (id) => {
+    let product = shopItemsData.find(prod => prod.id === id);
+    let basketItem = basket.find(item => item.id === id);
     
-    let search = basket.find(basketItem => basketItem.id === id);
-    if(!search) {
+    if(!basketItem) {
         return;
-    } else if(search.count === 0){
+    } else if(basketItem.count === 0){
         return;
     } else {
-        search.count -= 1;
+        basketItem.count -= 1;
+        basketItem.stock += 1;
+        product.stock += 1;
+
     }
 
     update(id);
+    
     basket = basket.filter((item) => item.count !== 0);
     localStorage.setItem("shop-products", JSON.stringify(basket));
+    localStorage.setItem("shop-stock", JSON.stringify(shopItemsData));
+
 };
 
 let update = (id) => {
